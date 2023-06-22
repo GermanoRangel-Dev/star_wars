f43c086cf342a1e9ec681cbd2d83ac042d16c37b
GermanoRangel-Dev <grangeldev@gmail.com>
2023-06-22 17:48:45 -0300

    commit 5
    Início do Javascript e criação dos cards pela url ainda de forma estática, mas controlando pela DOM.

---
+++ script.js
@@ -0,0 +1,47 @@
+let currentPageUrl = 'https://swapi.dev/api/people/';
+
+window.onload = async () => {
+  try{
+    await loadCharacters(currentPageUrl);
+  } catch (error) {
+    console.log(error);
+    alert('Erro ao carregar Cards!');
+  }
+};
+
+// retorno da url e criação dos cards dinâmicos
+
+async function loadCharacters(url) {
+  const mainContent = document.getElementById("main-content");
+  mainContent.innerHTML = ''; // limpar os campos cards e reutiliza 
+
+  try{
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    responseJson.results.forEach((character) => {
+      const card = document.createElement("div");
+      card.style.backgroundImage = `url('https://starwars-visualguide.com/assets/img/characters/1.jpg')`
+      card.className = "cards"
+
+      const characterNameBG = document.createElement("div");
+      characterNameBG.className = "character-name-bg";
+
+      const characterName = document.createElement("span");
+      characterName.className = "character-name";
+      characterName.innerText = `${character.name}`;
+
+      characterNameBG.appendChild(characterName);
+      card.appendChild(characterNameBG);
+
+      mainContent.appendChild(card);
+     });
+
+     currentPageUrl = url;
+
+  } catch (error){
+    alert('Erro ao carregar os novos personagens');
+    console.log(error);
+  }
+}
